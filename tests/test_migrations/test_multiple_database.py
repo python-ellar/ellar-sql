8e20ce5e72229fe719762ffac22447becb3d7a9e
Ezeudoh Tochukwu <ezeudoh.tochukwu@gmail.com>
2024-01-14 14:26:07 +0100

    Added test for Async Engine migrations

--- tests/test_migrations/test_multiple_database.py
+++ tests/test_migrations/test_multiple_database.py
@@ -54,3 +54,37 @@ def test_migrate_upgrade_multiple_database_with_model_changes():
                 b"Detected type change from VARCHAR(length=256) to String(length=128)"
                 in result.stderr
             )
+
+
+@clean_directory("multiple_async")
+def test_migrate_upgrade_for_multiple_database_async():
+    with set_env_variable("multiple_db", "true"):
+        result = run_command("multiple_database_async.py db init")
+        assert result.returncode == 0
+        assert (
+            b"tests/dumbs/multiple_async/migrations/alembic.ini' before proceeding."
+            in result.stdout
+        )
+
+        result = run_command("multiple_database_async.py db check")
+        assert result.returncode == 1
+
+        result = run_command("multiple_database_async.py db migrate")
+        assert result.returncode == 0
+
+        result = run_command("multiple_database_async.py db check")
+        assert result.returncode == 1
+
+        result = run_command("multiple_database_async.py db upgrade")
+        assert result.returncode == 0
+
+        result = run_command("multiple_database_async.py db check")
+        assert result.returncode == 0
+        assert result.stdout == b"No new upgrade operations detected.\n"
+
+        result = run_command("multiple_database_async.py add-user")
+        assert result.returncode == 0
+        assert (
+            result.stdout
+            == b"<User name=Multiple Database App Ellar id=1 and Group name=group id=1>\n"
+        )
